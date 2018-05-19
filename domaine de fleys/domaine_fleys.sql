f6687925b6b15725877c81cdb5ad25b0388a4dc5
Killrogue <38313066+Killrogue@users.noreply.github.com>
2018-05-19 22:35:33 +0200

    Mise à jour BDD
    Ajout des tables user et rank pour le module d'administration.

--- domaine de fleys/domaine_fleys.sql
+++ domaine de fleys/domaine_fleys.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mer. 02 mai 2018 à 08:48
+-- Généré le :  sam. 19 mai 2018 à 22:33
 -- Version du serveur :  10.1.31-MariaDB
--- Version de PHP :  7.2.3
+-- Version de PHP :  7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -38,23 +38,43 @@ CREATE TABLE `caracteristique` (
   `cepage` text NOT NULL,
   `age_des_vignes` text NOT NULL,
   `superficie` text NOT NULL,
-  `rendement` text NOT NULL,
   `production` text NOT NULL,
   `vendanges` text NOT NULL,
-  `exposition` varchar(50) NOT NULL
+  `medailles` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `caracteristique`
 --
 
-INSERT INTO `caracteristique` (`id`, `description`, `vinification`, `degustation`, `cepage`, `age_des_vignes`, `superficie`, `rendement`, `production`, `vendanges`, `exposition`) VALUES
-(1, 'Ce Chablis mis en bouteille par nos soins est issu de parcelles limitrophes de nos Chablis 1er Cru, des lieux-dits. Côte neuve et « Vaux-Noyers ».', 'Fermentation alcoolique sans sucre résiduel + fermentation malolactique 100% vinifié à 100% en cuve inox.', 'Robe jaune pâle avec quelques nuances paille et aussi des reflets verts de jeunesse, éclatante et limpide, belle présentation. Le nez est typé, il développe des notes de fleurs printanières et fleurs blanches de prairie avec en plus des fragrances sur les fruits blancs (la pomme et la pêche). L’aération ajoute une touche exotique et des notes de pain de mie ainsi qu’un arrière-plan discrètement minéral. Belle bouche avec une attaque relativement pleine, ronde soyeuse, qui restitue les arômes fruités du nez tout en y ajoutant touche, sur l’ananas et de jolis arômes acidulés qui lui donnent un esprit vif. A l’évolution on a un vin qui présente une belle matière avec du gras contrebalancé par une bonne fraîcheur. L’ensemble est équilibré avec une finale vivifiante sur des touches citronnées, d’une jolie netteté.', 'Chardonnay', '30 ans', '2 ha', '50 hL/ha', '14000 bouteilles', 'Mécanique', 'Ouest (Rive droite du Serein)'),
-(2, 'Ce Chablis est issu de parcelles limitrophes de nos Chablis premiers crus « Les Fourneaux »', 'Fermentation alcoolique sans sucre résiduel + fermentation malolactique 100%. 30% en fût de chêne (pièce bourguignonne 227L) de 1, 2 et 3 ans. 70% cuve inox.', 'Le nez associe les fleurs et les fruits blancs à des touches minérales et à un léger boisé épicé. A la fois ample, nerveuse et saline, la bouche développe des notes de pêche, de noisette, sans oublier ces touches de coquille d’huître typique du Chablis. La minéralité se fond dans la rondeur, la finale et longue et fraîche. Un vin « parfait pour les fruits de mer ».', 'Chardonnay', '40 ans', '2 ha', '50 hL/ha', '5500 bouteilles', 'Mécanique', 'Sud-Ouest (Rive droite du Serein)'),
-(3, 'Il s’agit de vignes attenantes au Château, fermées par un clos. Elles sont cultivées dans le plus grand respect de la nature, sans herbicide, raisonnée au maximum.  Avec sa cire sur le goulot, elle nous rappelle le bon vieux temps.', 'Une partie du vin est élevée en cuve ovoïdes (80%), une autre en fût de chêne (20%), ce qui donne un vin très équilibré.', 'Robe jaune paille, très brillante avec des reflets argentés, limpide, belle présentation avec même de la densité. Le nez est typé, fin et expressif, on perçoit des parfums de fleurs blanches (aubépine, acacia), de pêche et de citron légèrement confit ; viennent ensuite des fragrances de pain de mie, une discrète touche beurrée sur un fond minéral (notes fumées et iodées). En bouche on a un Chablis à l\'attaque ample, pleine qui restitue avec netteté le plan aromatique du nez. A l\'évolution, on a une matière plus grasse qui est enveloppée par une fraîcheur ciselée dans laquelle on retrouve ces parfums minéraux. L\'ensemble a un joli volume, c\'est harmonieux, précis avec une finale sur des notes beurrées, citronnées mais aussi une touche qui rappelle la coquille d\'huître.', 'Chardonnay', 'N/A', '1 ha', 'N/A', 'N/A', 'Manuelle', 'N/A'),
-(4, 'Ce Chablis 1er cru  « Mont de Milieu » est situé sur la rive droite du Serein, exposé plein Sud. Le Mont de Milieu est historique et prestigieux, pour ses vins d’une grande typicité « Chablis » ; marqué de minéralité.', 'Fermentation alcoolique, sans sucres+ fermentation malolactique. 100% vinifié et élevé à 100% en cuve inox afin de garder toute sa minéralité.', 'N/A', 'Chardonnay', '50 ans en moyenne', '2.5 ha', '50 hl/ha', '6600 bouteilles', 'Manuelle', 'Sud'),
-(5, 'Ce Chablis 1er cru « les Fourneaux » est situé sur la rive droite du Serein, exposé Sud et Sud Ouest, sur deux parcelles différentes, très ensoleillées, très argileuses, donnant des vins très fruités tout en gardant sa minéralité.', 'Fermentation alcoolique, sans sucres résiduels + fermentation malolactique 100%. Vinifié et élevé en cuve inox à 100%.', 'Robe jaune paille avec des reflets verts argentés, d\'une belle luminosité et limpidité, ainsi que quelques larmes. Le nez exprime immédiatement des parfums de fruits à chair blanche et jaune (poire, pêche de vigne, coing, abricot) à des notes de petites fleurs blanches de prairie. A ces caractéristiques du cépage s\'ajoutent des parfums citronnés et une dimension minérale (pierre à fusil, iode, coquille d\'huîtres). C\'est un nez précis qui se livre directement. En bouche même constat, c\'est un vin très jeune, droit, avec une certaine tension et un esprit vif et taquin. A l\'évolution, belle ossature avec une matière charnue bien parfumée qui est toujours contrebalancée par cette pointe de nervosité relativement plaisante. L\'ensemble est fin, dynamique et encore un peu fougueux, avec une finale où on retrouve ces notes citronnées et salines. ', 'Chardonnay', '50 ans en moyenne', '0.50 ha', '58 hl/ha', '2000 bouteilles', 'Manuelle', 'Sud et Sud Ouest'),
-(6, 'Le Mont de Milieu est un 1er cru de Chablis situé sur la rive droite du Serein, exposé plein Sud. Ce climat est historique et réputé pour ses vins d’une grande typicité « Chablis ». Ici il s’agit de vignes très vieilles, à rendement faibles, les vins sont concentrés sans perdre sa finesse et sa typicité.', 'Fermentation alcoolique sans sucres résiduels + fermentation malolactique 100%. 30% en fût de chêne (pièce bourguignonne) de 1, 2 et 3 ans. 70% cuve inox.', 'Le nez fin et expressif développe les arômes typés du chardonnay du nord de la Bourgogne avec des fleurs blanches et des accents minéraux ; viennent ensuite des parfums fruité. L’aération reste sur sa pureté minérale tout en y ajoutant des notes de beurre frais et une discrète touche vanillée. En bouche on retrouve la typicité et de la personnalité d\'un 1er Cru. L’attaque est ample, grasse et dotée d’une expression fruitée charnue. A l’évolution, on a une matière riche où le gras est contrebalancé par une fraîcheur ciselée et acidulée lui donnant un côté aérien.', 'Chardonnay', '80 ans (plantées en 1935 et 1936)', '0.70 ha', '30 hl/ha', '3000 bouteilles', 'Manuelle', 'Sud');
+INSERT INTO `caracteristique` (`id`, `description`, `vinification`, `degustation`, `cepage`, `age_des_vignes`, `superficie`, `production`, `vendanges`, `medailles`) VALUES
+(1, 'Ce Chablis mis en bouteille par nos soins est issu de parcelles limitrophes de nos Chablis 1er Cru, des lieux-dits. Côte neuve et « Vaux-Noyers ».', 'Fermentation alcoolique zéro sucre 100% en cuve inox.', 'Couleur : jaune pâle, nuance paille reflets verts de jeunesse\r\n<br>\r\n<br>\r\nNez : notes de fleurs printanières et fleurs blanches, fruits blancs\r\n<br>\r\n<br>\r\nAération : touche exotique, note de pain de mien peu minéral\r\n<br>\r\n<br>\r\nBouche : attaque ronde, soyeuse, esprit vif, belle matière \r\n<br>\r\n<br>\r\nFinal : équilibré, vivifiant avec des touches citronnés, jolie netteté', 'Chardonnay', ' environ 30 ans ', '1.30ha', '20 000 bouteilles', 'Mécanique', '\r\nassets\\images\\medaille-bronze.png\r\n'),
+(2, 'Ce Chablis est issu de parcelles limitrophes de nos Chablis premiers crus « Les Fourneaux »', 'Fermentation alcoolique zéro sucre 70% en cuve inox et 30% en fût de chêne(1,2 et 3ans).', 'Couleur : robe jaune pâle trés brillante reflets verts de jeunesse\r\n<br>\r\n<br>\r\nNez : notes fumées, minérales, fleurs blanches, fruit à chair blanche\r\n<br><br>\r\nAération : touche minéral, note de brioche\r\n<br><br>\r\nBouche : attaque ample, ronde, charnue\r\n<br><br>\r\nFinal : équilibré entre le gras et la fraîcheur', 'Chardonnay', 'Environ 40 ans', '2 ha', '8 000 bouteilles', 'Mécanique', 'assets/images/medaille-or.png'),
+(3, 'Il s’agit de vignes attenantes au Château, fermées par un clos. Elles sont cultivées dans le plus grand respect de la nature, sans herbicide, raisonnée au maximum.  Avec sa cire sur le goulot, elle nous rappelle le bon vieux temps.', 'Fermentation alcoolique zéro sucre 80% en cuve ovoïdes et 20% en fût de chêne.', 'Couleur : jaune paille, reflets argentés\r\n<br><br>\r\nNez : typé, fin, expressif, notes de fleurs blanches, pêche, citron, fond minéral \r\n<br><br>\r\nAération : Note de pain de mie, fond minéral\r\n<br><br>\r\nBouche : attaque ample\r\n<br><br>\r\nFinal : parfums minéraux, joli volume, précis avec une finale sur des notes beurrrées, citronnées', 'Chardonnay', 'environ 50 ans', '1 ha', '1 500 bouteilles', 'Manuelle, raisonné \"bio\"', 'assets/images/medaille-argent.png'),
+(4, 'Ce Chablis 1er cru  « Mont de Milieu » est situé sur la rive droite du Serein, exposé plein Sud. Le Mont de Milieu est historique et prestigieux, pour ses vins d’une grande typicité « Chablis » ; marqué de minéralité.', 'Fermentation alcoolique zéro sucre 100% cuve inox.', 'Couleur : jaune clair \r\n<br><br>\r\nNez : rafinné, trés minéral, soutenu par des fruits secs\r\n<br><br>\r\nAération : note de brioche, minéral qui rapelle la pierre de fusil et le côté iodé\r\n<br><br>\r\nBouche : attque ronde et charnue, soutenue par une belle fraîcheur\r\n<br><br>\r\nFinal : persistante sur les fruits exotiques et touche minéral\r\n', 'Chardonnay', 'Environ 50 ans', '2.5 ha', '7 000 bouteilles', 'Manuelle', ''),
+(5, 'Ce Chablis 1er cru « les Fourneaux » est situé sur la rive droite du Serein, exposé Sud et Sud Ouest, sur deux parcelles différentes, très ensoleillées, très argileuses, donnant des vins très fruités tout en gardant sa minéralité.', 'Fermentation alcoolique zéro sucre 100% cuve inox.', 'Couleur : jaune claire, nuances ors blancs, reflets verts\r\n<br><br>\r\nNez : fin, expressif, notes florales, touches de fruits blancs, citron \r\n<br><br>\r\nAération : fragances minérales, notes fraîche, charnue \r\n<br><br>\r\nBouche : parfums floraux et fruités\r\n<br><br>\r\nFinal : côté beurré et minéral, finale fumée, touche iodée', 'Chardonnay', 'Environ 50 ans ', '0.50 ha', '3 900 bouteilles', 'Manuelle', 'assets/images/medaille.png'),
+(6, 'Le Mont de Milieu est un 1er cru de Chablis situé sur la rive droite du Serein, exposé plein Sud. Ce climat est historique et réputé pour ses vins d’une grande typicité « Chablis ». Ici il s’agit de vignes très vieilles, à rendement faibles, les vins sont concentrés sans perdre sa finesse et sa typicité.', 'Fermentation alcoolique zéro sucre 70% cuve inox et 30% fût de chêne.', 'Couleur : jaune, note paille argentées\r\n<br><br>\r\nNez : net, fin, parfums de fruits blancs, notes florales, herbe coupée\r\n<br><br>\r\nAération : plus riches, notes d\'agrumes, beurre frais, minéral\r\n<br><br>\r\nBouche : ample, pleine, bien parfumée\r\n<br><br>\r\nFinal : minérale, note grillée et parfumées', 'Chardonnay', 'Environ 83 ans(plantées en 1935 et 1936)', '0.70 ha', '2 600 bouteilles', 'Manuelle', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rank`
+--
+
+CREATE TABLE `rank` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `niveau_autorisation` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `rank`
+--
+
+INSERT INTO `rank` (`id`, `nom`, `niveau_autorisation`) VALUES
+(1, 'Admin', 99),
+(2, 'Modérateur', 98),
+(3, 'Utilisateur', 1);
 
 -- --------------------------------------------------------
 
@@ -80,6 +100,28 @@ INSERT INTO `slider` (`id`, `lien_image`, `texte_alt`, `caption`) VALUES
 (4, 'assets/images/slider4.jpg', 'Enseigne du domaine', 'Enseigne du domaine'),
 (5, 'assets/images/slider5.jpg', 'Vendanges manuelles', 'Vendanges manuelles'),
 (6, 'assets/images/slider6.jpg', 'Caveau du domaine', 'Caveau du domaine');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `motdepasse` varchar(64) NOT NULL,
+  `id_rang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `nom`, `email`, `motdepasse`, `id_rang`) VALUES
+(1, 'Anthony', 'anthony.carrer@hotmail.fr', '08d49d535999603006c85c7189552163', 1),
+(2, 'aaa', 'aaa@aa.fr', '47bce5c74f589f4867dbd57e9ca9f808', 2);
 
 -- --------------------------------------------------------
 
@@ -119,10 +161,23 @@ ALTER TABLE `caracteristique`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `rank`
+--
+ALTER TABLE `rank`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `slider`
 --
 ALTER TABLE `slider`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_rang` (`id_rang`);
 
 --
 -- Index pour la table `vin`
@@ -142,10 +197,22 @@ ALTER TABLE `caracteristique`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT pour la table `rank`
+--
+ALTER TABLE `rank`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT pour la table `slider`
 --
 ALTER TABLE `slider`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `vin`
@@ -156,6 +223,12 @@ ALTER TABLE `vin`
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_rang`) REFERENCES `rank` (`id`);
 
 --
 -- Contraintes pour la table `vin`
